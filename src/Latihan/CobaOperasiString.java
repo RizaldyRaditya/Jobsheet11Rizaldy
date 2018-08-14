c639a4aa5f43cf69ec36da37eed2e65085d91daf
Rudi Erwanto <Rudi Erwanto@DESKTOP-86HM3LO>
2018-08-14 14:19:55 +0700

    Coba method length & isEmpty

--- src/Latihan/CobaOperasiString.java
+++ src/Latihan/CobaOperasiString.java
@@ -14,5 +14,16 @@ public class CobaOperasiString {
     public static void main(String[] args) {
         String identitas = "Rizaldy Raditya Erwamto / X RPL 5 / 36";
         System.out.println("identitas : "+ identitas);
+        
+        String x = " Operasi";
+        System.out.println("isi variabel x : "+ x);
+        System.out.println("\""+ x +"\" panjang karakter = "+ x.length());
+        
+        System.out.println("x adalah kosong : " + x.isEmpty());
+        
+        String y = "";
+        System.out.println("isi variabel y : "+ y);
+        System.out.println("y adalah kosong : " + y.isEmpty());
+        
     }
 }

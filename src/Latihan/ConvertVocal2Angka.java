5dbefd743289f4d9c70e0476d76c2927d94a7b7c
Rudi Erwanto <Rudi Erwanto@DESKTOP-86HM3LO>
2018-08-15 08:17:51 +0700

    Initial Commit

---
+++ src/Latihan/ConvertVocal2Angka.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author Rudi Erwanto
+ */
+public class ConvertVocal2Angka {
+    private static void tampilJudul(String identitas) {
+        System.out.println("Identitas : " + identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+    }
+    String identitas = "Rizaldy Raditya Erwanto / XRPL 5 / 36";
+    
+    tampilJudul(identitas);
+    
+    
+}

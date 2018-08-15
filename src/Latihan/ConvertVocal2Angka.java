65ebb1561b40d16a53e6645d7f879c8263fea51a
Rudi Erwanto <Rudi Erwanto@DESKTOP-86HM3LO>
2018-08-15 09:53:24 +0700

    Tambah Konversi

--- src/Latihan/ConvertVocal2Angka.java
+++ src/Latihan/ConvertVocal2Angka.java
@@ -21,6 +21,7 @@ public class ConvertVocal2Angka {
         String identitas = "Rizaldy Raditya Erwanto / X RPL 5 / 36";
         tampilJudul(identitas);
         String kalimat = tampilInput();
+        String convert = vocal2Angka(kalimat);
     }
     private static String tampilInput () {
         Scanner scanner = new Scanner(System.in);
@@ -30,5 +31,16 @@ public class ConvertVocal2Angka {
         System.out.println("Kalimat Asli : " + kalimat);
         
         return kalimat;
+    }
+    private static String vocal2Angka(String kalimat) {
+        char [][] arConvert = 
+        {{'a','4'},{'i','1'}, {'u','2'}, {'e','3'}, {'o', '0'}};
+        
+        kalimat = kalimat.toLowerCase();
+      for(int i=0;i<arConvert.length;i++)
+          kalimat = kalimat.replace(arConvert [i][0], arConvert[i][1]);
+        
+        return kalimat;
+        
     }
 }
